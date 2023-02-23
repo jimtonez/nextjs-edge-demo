ad9cd1eb581f037ad0ae64035545bc3ac0a31bdd
Tim Jones <timjones@redhat.com>
2023-02-23 14:46:16 -0700

    adding team logo and score content to top of events

--- components/Events.tsx
+++ components/Events.tsx
@@ -14,13 +14,25 @@ function Events () {
                 <ul className='list-none flex flex-col space-y-4'>
                     {events.map((event, index) => (
                         <li key={event.id} className='cursor-pointer'>
+                            <div className='flex flex-col aspect-w-8 aspect-h-2'>
+                                <div className='flex w-auto h-auto items-center justify-center rounded-t-xl border border-b-none border-gray-700 p-2 lg:p-4 space-x-4'>
+                                    {event.teams?.map((team, index) => (
+                                        <div key={team.name} className='flex flex-col text-white w-full h-full items-center justify-center border border-gray-700 border-dashed rounded-xl p-2 lg:p-4'>
+                                            <div className={`${index === 1 ? 'hidden' : 'flex'} h-12 lg:h-14 w-12 lg:w-14 rounded-full border border-red-600 border-2`}>
+                                                <Image src={team.logo} alt="" className="object-cover h-auto w-auto rounded-full" />
+                                            </div>
+                                            <p className={`${index === 1 ? 'text-2xl font-bold' : 'flex'} line-clamp-1`}>{team.name}</p>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
                             <div className='flex flex-col aspect-w-3 aspect-h-2'>
                                 <div className='flex'>
-                                    <iframe key={event.id} id={event.id} className=" md:rounded-xl md:rounded-b-none" width="100%" height="100%" src={event.url} />
+                                    <iframe key={event.id} id={event.id} className="" width="100%" height="100%" src={event.url} />
                                 </div>
                             </div>
                             <div className='flex flex-col aspect-w-3 sm:aspect-w-2 aspect-h-2 sm:aspect-h-1'>
-                                <div className='flex flex-col bg-[#070D0D] rounded-b-xl border border-gray-700 border-t-0 hover:bg-opacity-80 p-4 space-y-4'>
+                                <div className='flex flex-col bg-[#070D0D] rounded-b-xl border border-gray-700 border-t-0 hover:bg-opacity-80 p-2 lg:p-4 space-y-4'>
                                     <div className='flex flex-row w-full h-auto items-center space-x-2'>
                                         <div className='flex w-1/8'>
                                             <div className="flex h-12 lg:h-14 w-12 lg:w-14 rounded-full border border-red-600 border-2">
@@ -32,7 +44,7 @@ function Events () {
                                         </div>
                                     </div>
                                     <div className='flex flex-row items-center justify-between space-x-2'>
-                                        {event.tags.map((tag) => (
+                                        {event.tags?.map((tag) => (
                                             <div key={tag.name} className='flex h-6 lg:h-8 w-auto items-center justify-between bg-gray-700 border border-red-600 rounded-full space-x-3 p-3'>
                                                 <div className='w-1/4'>
                                                     <p>{tag.icon}</p>
@@ -44,11 +56,11 @@ function Events () {
                                         ))}
                                     </div>
                                     <div className='flex flex-row w-full h-3/4 items-center justify-between space-x-2'>
-                                        <div className='flex flex-col w-full h-full border border-gray-700 border-dashed rounded-xl'>
+                                        {/* <div className='flex flex-col w-full h-full border border-gray-700 border-dashed rounded-xl'>
                                             <div className='flex h-1/4 w-full items-center justify-center rounded-t-xl border-b border-gray-700 border-dashed'>
                                                 <h1 className='text-white'>Score</h1>
                                             </div>
-                                        </div>
+                                        </div> */}
                                         <div className='flex w-full h-full border border-gray-700 border-dashed rounded-xl'>
                                              <div className='flex h-1/4 w-full items-center justify-center rounded-t-xl border-b border-gray-700 border-dashed'>
                                                 <h1 className='text-white'>Spread</h1>
--- constants/index.tsx
+++ constants/index.tsx
@@ -1,5 +1,9 @@
 import nbcsports from '../public/nbcsports.jpeg'
 import golfch from '../public/golfch.jpeg'
+import manchesterUnited from '../public/manchester_united.png'
+import leicester from '../public/leicester.png'
+import liverpool from '../public/liverpool.png'
+import newcastle from '../public/newcastle.png'
 
 export const events = [
     {
@@ -12,20 +16,35 @@ export const events = [
                 icon: "⚽️"
             },
         ],
+        teams: [
+            {
+                name: "Manchester United",
+                logo: manchesterUnited
+            },
+            {
+                name: "3-0",
+                logo: leicester
+            },
+            {
+                name: "Leicester City",
+                logo: leicester
+            }
+        ],
+        score: "3-0",
         url: "https://www.youtube.com/embed/V1n0Da6zbeM"
     },
-    {
-        id: "event-2",
-        title: "Relive Tiger Woods' best shots from Rd. 3 at Genesis Invitational | Golf Channel",
-        logo: golfch,
-        tags: [
-            {
-                name: "Golf",
-                icon: "⛳️"
-            },
-        ],
-        url: "https://www.youtube.com/embed/IORHuqDy9rs"
-    },
+    // {
+    //     id: "event-2",
+    //     title: "Relive Tiger Woods' best shots from Rd. 3 at Genesis Invitational | Golf Channel",
+    //     logo: golfch,
+    //     tags: [
+    //         {
+    //             name: "Golf",
+    //             icon: "⛳️"
+    //         },
+    //     ],
+    //     url: "https://www.youtube.com/embed/IORHuqDy9rs"
+    // },
     {
         id: "event-3",
         title: "Newcastle United v. Liverpool | PREMIER LEAGUE HIGHLIGHTS | 2/18/2023 | NBC Sports",
@@ -36,6 +55,21 @@ export const events = [
                 icon: "⚽️"
             },
         ],
+        teams: [
+            {
+                name: "Newcastle United",
+                logo: newcastle
+            },
+            {
+                name: "0-2",
+                logo: leicester
+            },
+            {
+                name: "Liverpool",
+                logo: liverpool
+            }
+        ],
+        score: "3-1",
         url: "https://www.youtube.com/embed/w3zQkhKImHk"
     }
 ];
