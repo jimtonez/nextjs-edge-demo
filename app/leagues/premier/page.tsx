41112cf1587e185405cc2dd1ce41dae7cd2bc89f
Tim Jones <timjones@redhat.com>
2023-03-11 18:11:56 -0700

    adding dynamic routing for NHL teams

--- app/leagues/premier/page.tsx
+++ app/leagues/premier/page.tsx
@@ -50,10 +50,10 @@ export default function Premier() {
         <LeagueHeader logo="🏴󠁧󠁢󠁥󠁮󠁧󠁿" name="Premier League" border="border-purple-700" />
         <section className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl lg:grid-cols-3 lg:max-w-5xl xl:grid-cols-4 xl:max-w-6xl mx-auto gap-4 px-2 lg:px-4 mt-4">
         <div className='col-span-1 md:col-span-2 lg:col-span-3 xl:col-span-4'>
-            <div className="flex flex-row w-full items-center justify-start space-x-2 pb-2 md:pb-4 border-b border-gray-700">
-                <button onClick={() => setTabState("upcoming")} className="text-sm text-gray-600 hover:bg-gray-600 hover:text-white h-8 w-auto px-2 border border-gray-600 hover:border-none rounded-full">Upcoming Events</button>
-                <button onClick={() => setTabState("teams")} className="text-sm text-gray-600 hover:bg-gray-600 hover:text-white h-8 w-auto px-2 border border-gray-600 hover:border-none rounded-full">Teams</button>
-            </div>
+          <div className="flex flex-row w-full items-center justify-start space-x-2 pb-2 md:pb-4 border-b border-gray-700">
+              <button onClick={() => setTabState("upcoming")} className={`${tabState === 'upcoming' ? 'bg-gray-600' : ''} text-sm  hover:bg-gray-600 text-white h-8 w-auto px-2 border border-gray-700 rounded-full`}>Upcoming Events</button>
+              <button onClick={() => setTabState("teams")} className={`${tabState === 'teams' ? 'bg-gray-600' : ''} text-sm hover:bg-gray-600 text-white h-8 w-auto px-2 border border-gray-700 rounded-full`}>Teams</button>
+          </div>
         </div>
         </section>
         {session ? (
