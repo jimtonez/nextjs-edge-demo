3da5ad89504116acdd2ea71ca7f1d2ed3cfc7687
Tim Jones <timjones@redhat.com>
2023-03-29 19:56:42 -0600

    disabling kafka streams

--- pages/api/bets/bets.js
+++ pages/api/bets/bets.js
@@ -1,27 +1,27 @@
-import { KafkaStreams } from 'kafka-streams'
+// import { KafkaStreams } from 'kafka-streams'
 
-export default async (req, res) => {
+// export default async (req, res) => {
 
-    const config = require("../../../backend/config/config.json")
-    const kafkaStreams = new KafkaStreams(config);
-    kafkaStreams.on("error", (error) => console.error(error));
+//     const config = require("../../../backend/config/config.json")
+//     const kafkaStreams = new KafkaStreams(config);
+//     kafkaStreams.on("error", (error) => console.error(error));
 
-    // const betsStream =
-    // factory.getKStream()
-    // .from("bets-in")
-    // .mapJSONConvenience()
-    // .mapWrapKafkaValue()
-    // .forEach(console.log);
+//     // const betsStream =
+//     // factory.getKStream()
+//     // .from("bets-in")
+//     // .mapJSONConvenience()
+//     // .mapWrapKafkaValue()
+//     // .forEach(console.log);
 
-    const kafkaTopicName = "bets-in";
-    const stream = kafkaStreams.getKStream(kafkaTopicName);
+//     const kafkaTopicName = "bets-in";
+//     const stream = kafkaStreams.getKStream(kafkaTopicName);
 
-    stream.forEach(message => console.log(message));
-    stream.start().then(() => {
-        console.log("stream started, as kafka consumer is ready.");
-    }, error => {
-        console.log("streamed failed to start: " + error);
-    });
+//     stream.forEach(message => console.log(message));
+//     stream.start().then(() => {
+//         console.log("stream started, as kafka consumer is ready.");
+//     }, error => {
+//         console.log("streamed failed to start: " + error);
+//     });
 
-    console.log(stream)
-}
+//     console.log(stream)
+// }
