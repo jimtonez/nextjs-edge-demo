c049e990debf0fb5578ad1a36b4e4772dc6c096b
Tim Jones <timjones@redhat.com>
2023-04-04 11:13:38 -0600

    changing method to post

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -2,14 +2,14 @@ import axios from "axios";
 
 export default async (req, res) => {
     switch (req.method) {
-        case "GET":
-            const getUser = await axios.get(`https://route-ed-quarkus-backend-ssl-cc-betting-demo.apps.cluster-r8szc.r8szc.sandbox1991.opentlc.com/customer/membership`, { params: req.body })
+        case "POST":
+            const getUser = await axios.post(`https://route-ed-quarkus-backend-ssl-cc-betting-demo.apps.cluster-r8szc.r8szc.sandbox1991.opentlc.com/customer/membership`, { params: req.body })
             // const getUser = await axios.get(`http://localhost:8080/customer?email=${encodeURIComponent(session.user.email)}`)
             res.status(200).json({ data: getUser.data })
-        case "POST":
-            const response = await axios.post(`http://localhost:8080/customer`, req.body);
-            res.status(200).json({ data: response.data })
-            break;
+        // case "POST":
+        //     const response = await axios.post(`https://route-ed-quarkus-backend-ssl-cc-betting-demo.apps.cluster-r8szc.r8szc.sandbox1991.opentlc.com/customer`, req.body);
+        //     res.status(200).json({ data: response.data })
+        //     break;
         // default:
         //     res.setHeader("Allow", ["GET", "POST"]);
         //     res.status(405).end(`Method ${req.method} Not Allowed`);
