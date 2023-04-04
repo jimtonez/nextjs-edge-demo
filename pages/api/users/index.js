01084ed7ab4091ef9a17833f35debb5adfc6f2b6
Tim Jones <timjones@redhat.com>
2023-04-04 12:35:52 -0600

    changing request params

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async (req, res) => {
     switch (req.method) {
         case "GET":
-            const getUser = await axios.get(`https://route-ed-quarkus-backend-ssl-cc-betting-demo.apps.cluster-r8szc.r8szc.sandbox1991.opentlc.com/customer/membership`, {"email": "test@test.com"} )
+            const getUser = await axios.get(`https://route-ed-quarkus-backend-ssl-cc-betting-demo.apps.cluster-r8szc.r8szc.sandbox1991.opentlc.com/customer/membership`, { params: {"email": "test@test.com"} } )
             // const getUser = await axios.get(`http://localhost:8080/customer?email=${encodeURIComponent(session.user.email)}`)
             res.status(200).json({ data: getUser.data })
         // case "POST":
