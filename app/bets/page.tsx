e164d9dd8eff1027ca24afc0d51841661bf1db07
Tim Jones <timjones@redhat.com>
2023-04-04 10:58:27 -0600

    testing api request proxy to backend

--- app/bets/page.tsx
+++ app/bets/page.tsx
@@ -1,9 +1,11 @@
+"use client"
 import { Inter } from '@next/font/google'
 import Events from '../../components/Events'
 import Stack from '../../components/Stack'
 import Bets from '@/components/Bets'
 import TabList from '@/components/TabList'
 import Upcoming from '@/components/Upcoming'
+import { RecoilRoot } from 'recoil'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,8 +13,10 @@ export default function Home() {
   return (
     <main className='flex h-screen flex-col'>
       <div className='space-y-4'>
-        <Bets />
-        <Stack />
+        <RecoilRoot>
+          <Bets />
+          <Stack />
+        </RecoilRoot>
       </div>
     </main>
   )
--- components/BetSlip.tsx
+++ components/BetSlip.tsx
@@ -25,12 +25,22 @@ function BetSlip () {
         setBets((bets) => bets.filter((_, i) => i !== index))
     }
 
-    // const getUser = await axios.get(`http://localhost:8080/customer?email=${encodeURIComponent(session.user.email)}`)
+    const userData = {
+        email: session?.user?.email
+    }
 
     async function handlePayment () {
         // const getUser = await axios.get(`http://svc-ed-quarkus-backend.cc-betting-demo.svc.cluster.local/customer/membership?email=${encodeURIComponent(session?.user?.email!)}`)
-        const getUser = await axios.get(`https://route-ed-quarkus-backend-ssl-cc-betting-demo.apps.cluster-r8szc.r8szc.sandbox1991.opentlc.com/customer/membership`, { params: {email: session?.user?.email!} })
-        console.log(getUser)
+        // const getUser = await axios.get(`https://route-ed-quarkus-backend-ssl-cc-betting-demo.apps.cluster-r8szc.r8szc.sandbox1991.opentlc.com/customer/membership`, { params: {email: session?.user?.email!} })
+        const response = await fetch('/api/users', {
+            method: 'GET',
+            body: JSON.stringify(userData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = await response.json()
+        console.log(data)
         setPayment(true)
         for (const bet of bets) {
             setBets((bets) => bets.filter((_, i) => i !== i ))
